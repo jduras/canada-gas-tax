3937279607c6c1a4916a88a01bb81b6b5cf53dfc
jduras <jan.duras@gmail.com>
2019-03-14 01:33:49 -0500

    updated gas tax data to 2018

--- src/c02_scrape_and_clean_tax_data.R
+++ src/c02_scrape_and_clean_tax_data.R
@@ -1,38 +1,52 @@
 
 # scrape tax data from https://charting.kentgroupltd.com/
-tax_tbl_raw <-
+tax_tbl_2016_raw <-
     "https://charting.kentgroupltd.com/WPPS/Taxation/Taxation%20Tables/HISTORICAL%20TAXATION/2016/Taxation_Taxation%20Tables_HISTORICAL%20TAXATION_2016.htm" %>%
     read_html() %>%
     html_table() %>%
     purrr::pluck(1)
 
-tax_tbl_raw %>%
+tax_tbl_2016_raw %>%
     glimpse()
 
-tax_tbl_raw %>%
+tax_tbl_2016_raw %>%
     slice(-(1:3)) %>%
     glimpse()
 
-tax_tbl_clean <-
-    tax_tbl_raw %>%
+tax_tbl_2016_clean <-
+    tax_tbl_2016_raw %>%
     mutate(X1 = if_else(row_number() == 4, "X1", X1)) %>%
     set_colnames(slice(., 4)) %>%
     slice(-c(1:4)) %>%
     mutate(group = (X1 == "") %>% cumsum()) %>%
     group_by(group) %>%
-    mutate(state = nth(X1, 2)) %>%
+    mutate(state_name = nth(X1, 2)) %>%
     ungroup() %>%
-    gather(date, value, -c(X1, group, state)) %>%
+    gather(date, value, -c(X1, group, state_name)) %>%
     filter(value != "") %>%
-    mutate(date = as.Date(date),
+    mutate(state = recode(state_name,
+                          Yukon = "YT",
+                          `British Columbia` = "BC",
+                          `North-West Territory` = "NT",
+                          Alberta = "AB",
+                          Saskatchewan = "SK",
+                          Manitoba = "MB",
+                          Ontario = "ON",
+                          Québec = "QC",
+                          `New Brunswick` = "NB",
+                          `Nova Scotia` = "NS",
+                          `Prince Edward Island` = "PE",
+                          Newfoundland = "NL",
+                          CANADA = "CA"),
+           date = as.Date(date),
            value = str_replace(value, "%", "") %>% parse_double()) %>%
     filter(str_detect(X1, "Gasoline")) %>%
-    select(state, date, tax = value)
+    select(state, state_name, date, tax = value)
 
-tax_tbl_clean %>%
+tax_tbl_2016_clean %>%
      #filter(state %in% c("BC", "AB", "SK", "MB")) %>%
-    filter(state %in% c("British Columbia", "Alberta", "Saskatchewan", "Manitoba")) %>%
-    ggplot(aes(x = date, y = tax, color = state)) +
+    filter(state_name %in% c("British Columbia", "Alberta", "Saskatchewan", "Manitoba")) %>%
+    ggplot(aes(x = date, y = tax, color = state_name)) +
         geom_point() +
         geom_step()
 
@@ -56,11 +70,33 @@ tax_tbl_clean <-
     slice(-c(1:4)) %>%
     mutate(group = is.na(X1) %>% cumsum()) %>%
     group_by(group) %>%
-    mutate(state = nth(X1, 2)) %>%
+    mutate(state_name = nth(X1, 2)) %>%
     ungroup() %>%
-    gather(date, value, -c(X1, group, state)) %>%
+    gather(date, value, -c(X1, group, state_name)) %>%
     filter(value != "") %>%
-    mutate(date = as.Date(date),
+    mutate(state = recode(state_name,
+                          Yukon = "YT",
+                          `British Columbia` = "BC",
+                          `North-West Territory` = "NT",
+                          Alberta = "AB",
+                          Saskatchewan = "SK",
+                          Manitoba = "MB",
+                          Ontario = "ON",
+                          Quebec = "QC",
+                          `New Brunswick` = "NB",
+                          `Nova Scotia` = "NS",
+                          `Prince Edward Island` = "PE",
+                          Newfoundland = "NL",
+                          CANADA = "CA"),
+           date = as.Date(date),
            value = str_replace(value, "%", "") %>% parse_double()) %>%
     filter(str_detect(X1, "Gasoline")) %>%
-    select(state, date, tax = value)
+    select(state, state_name, date, tax = value)
+
+tax_tbl_clean %>%
+    #filter(date >= "2013-01-01", date <= "2017-12-31") %>%
+    #filter(state %in% c("BC", "AB", "SK", "MB")) %>%
+    filter(state_name %in% c("British Columbia", "Alberta", "Saskatchewan", "Manitoba")) %>%
+    ggplot(aes(x = date, y = tax, color = state_name)) +
+        geom_point() +
+        geom_step()
